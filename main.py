07c5a7d16168a46c623a364d66cf66c8895ce730
TheKaplumbag <scpmtf4@gmail.com>
2025-09-22 22:06:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import discord
+from discord import app_commands
+import os
+from dotenv import load_dotenv
+from discord.ext import commands
+load_dotenv(".env")
+TOKEN = os.getenv("TOKEN")
 
+bot= commands.Bot(command_prefix="!",intents=discord.Intents.all()) 
+@bot.event
+async def on_message(message):
+    if message.content.startswith("sa MZAR"):
+        await message.channel.send("**Aleyküm Selam Hoşgeldin !**")
+
+@bot.event
+async def on_ready():
+    print("BOT ÇALIŞIYOR")
+bot.run(TOKEN)
