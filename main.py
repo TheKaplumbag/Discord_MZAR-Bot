2af1578cff35d8f5cf83691d928f9a1f9c66b194
TheKaplumbag <scpmtf4@gmail.com>
2025-09-23 09:03:40 +0300

    1 slash komutu eklendi

--- main.py
+++ main.py
@@ -2,18 +2,29 @@ import discord
 from discord import app_commands
 import os
 from dotenv import load_dotenv
-from discord import discord.ext
 from discord.ext import commands
 load_dotenv(".env")
 TOKEN = os.getenv("TOKEN")
 
-bot= commands.Bot(command_prefix="!",intents=discord.Intents.all()) 
-@bot.event
-async def on_message(message):
-    if message.content.startswith("sa MZAR"):
-        await message.channel.send("**Aleyküm Selam Hoşgeldin !**")
+@bot.tree.command(name="TEST1",desciption="MZAR TEST1")
+async def test(interaction: discord.Interaction):
+    channel = interaction.channel
+        user = interaction.user
+            await interaction.response.send_message(f"{user.mention} Selam!")
 
-@bot.event
-async def on_ready():
-    print("BOT ÇALIŞIYOR")
-bot.run(TOKEN)
+
+
+            bot= commands.Bot(command_prefix="!",intents=discord.Intents.all()) 
+            @bot.event
+            async def on_message(message):
+                if message.content.startswith("sa MZAR"):
+                        await message.channel.send("**Aleyküm Selam Hoşgeldin !**")
+
+
+
+
+
+                        @bot.event
+                        async def on_ready():
+                            print("BOT ÇALIŞIYOR")
+                            bot.run(TOKEN)
